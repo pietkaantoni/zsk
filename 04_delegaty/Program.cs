06c716432d095c174a2ac8b19430feb1898f4286
pietkaantoni <144986012+pietkaantoni@users.noreply.github.com>
2024-11-20 15:41:45 +0100

    Add files via upload

---
+++ 04_delegaty/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_delegaty
+{
+    internal class Program
+    {
+        public delegate float Operation(float x, float y);
+
+        public static float Add(float x, float y)
+        {
+            return x + y;
+        }
+        public static float Substract(float x, float y)
+        {
+            return x - y;
+        }
+        public static float Multuply(float x, float y)
+        {
+            return x * y;
+        }
+        public static float Divide(float x, float y)
+        {
+            return x / y;
+        }
+
+        public static void DisplayResult(Operation op, float x, float y)
+        {
+            float result;
+
+            if (op.Method.Name == "Divide" && y == 0)
+            {
+                Console.WriteLine("Błędne dzielenie przez 0");
+                result = 0;
+            }
+            else
+            {
+                try
+                {
+                    result = op(x, y);
+                    Console.WriteLine("Wynik operacji {0} na liczbach {1} i {2} wynosi {3}", op.Method.Name, x, y, result);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                    result = 0;
+                }
+            }
+        }
+
+        public static float GetFloatFromUser(string prompt)
+        {
+            Console.WriteLine(prompt);
+            float number;
+
+            bool isValid = float.TryParse(Console.ReadLine(), out number);
+
+            while(!isValid)
+            {
+                Console.Write("Nieprawidłowe dane. Podaj liczbę:");
+                isValid = float.TryParse(Console.ReadLine(), out number);
+            }
+            return number;
+        }
+
+        static void Main(string[] args)
+        {
+            //kod bardziej elastyczny (łatwo dodawać nowe operacje) i łatwy do utrzymania dzięki Operacjom, delegatom
+            float a = GetFloatFromUser("Podaj pierwszą liczbę:");
+            float b = GetFloatFromUser("Podaj drugą liczbę:");
+
+            Operation adding = new Operation(Add);
+            Operation substracting = new Operation(Substract);
+            Operation multuplying = new Operation(Multuply);
+            Operation dividing = new Operation(Divide);
+
+            DisplayResult(adding, a, b);
+            DisplayResult(substracting, a, b);
+            DisplayResult(multuplying, a, b);
+            DisplayResult(dividing, a, b);
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 04_delegaty/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("04_delegaty")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("04_delegaty")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("edcffa4c-ac9c-4b3e-a0d1-182fdca3abd6")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
