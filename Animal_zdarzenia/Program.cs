0c9bee5bbad2d666209028c812a12cd31bbcb3c4
Antoni Piętka <144986012+pietkaantoni@users.noreply.github.com>
2025-03-06 07:55:34 +0100

    Add files via upload

---
+++ Animal_zdarzenia/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal_zdarzenia
+{
+    internal class Program
+    {
+        public enum AnimalSpecies
+        {
+            Dog,
+            Cat,
+            Lion,
+            Fish
+        }
+        
+
+        public class Animal
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public AnimalSpecies Species { get; set; }
+            public List<string> Traits { get; set; }
+            public Dictionary<string, string> Attributes { get; set; }
+            public bool IsWild { get; set; }
+
+            public event Action<string> AnimalCreated;
+            public event Action<string> AnimalTraitAdded;
+            public event Action<string> AnimalAttributeAdded;
+            public event Action<string> AnimalStatusChange;
+
+            public Animal(string name, int age, AnimalSpecies species, bool isWild)
+            {
+                this.Name = name;
+                this.Age = age;
+                this.Species = species;
+                this.IsWild = isWild;
+                Traits = new List<string>();
+                Attributes = new Dictionary<string, string>();
+                AnimalCreated?.Invoke($"Stworzono zwierzę: {this.Name}");
+            }
+            public void AddTrait(string trait)
+            {
+                Traits.Add(trait);
+                AnimalTraitAdded?.Invoke($"Dodano nowy trait: {trait}");
+            }
+            public void AddAttribute(string key, string value)
+            {
+                Attributes.Add(key, value);
+                AnimalTraitAdded?.Invoke($"Dodano nowy attrybut: {key}: {value}");
+            }
+            public void ChangeStatus(bool isWild)
+            {
+                this.IsWild=isWild;
+                string status = isWild ? "Dziki" : "Nie dziki";
+                AnimalStatusChange?.Invoke($"Zmienionio status na {status}");
+            }
+            public void ShowInfo()
+            {
+                Console.WriteLine($"{this.Name}, {this.Age} years old, dziki: {this.IsWild}, Traits: {string.Join(", ", Traits)}");
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Animal cat = new Animal("Puszek", 2, AnimalSpecies.Cat, false);
+            cat.AnimalCreated += message => Console.WriteLine(message);
+            cat.AnimalTraitAdded += message => Console.WriteLine(message);
+            cat.AnimalAttributeAdded += message => Console.WriteLine(message);
+            cat.AnimalStatusChange += message => Console.WriteLine(message);
+            cat.AddTrait("Powerful");
+            cat.AddTrait("Cute");
+            cat.AddAttribute("Color", "Grey");
+            cat.ChangeStatus(false);
+            cat.ShowInfo();
+            Console.WriteLine("Koniec Programu");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Animal_zdarzenia/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("Animal_zdarzenia")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Animal_zdarzenia")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("42c54ec0-3854-43b4-bfa2-36ccd6b6a3de")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
